426e4191432b5c69dad100bf1fc984d8f810b0b3
Majzik Zoltán <zoltan.majzik@quantislabs.com>
2014-04-15 22:00:22 +0200

    filters added to public classes

--- protected/controllers/PositionController.php
+++ protected/controllers/PositionController.php
@@ -2,6 +2,17 @@
 
 class PositionController extends GxController {
 
+	/**
+	 * @return array action filters
+	 */
+	public function filters()
+	{
+		return array(
+			'accessControl', // perform access control for CRUD operations
+			'postOnly + delete', // we only allow deletion via POST request
+		);
+	}
+	
 	public function actionIndex() {
 		$model = new Position('search');
 		$model->unsetAttributes();
--- protected/controllers/StationController.php
+++ protected/controllers/StationController.php
@@ -2,6 +2,17 @@
 
 class StationController extends GxController {
 
+	/**
+	 * @return array action filters
+	 */
+	public function filters()
+	{
+		return array(
+			'accessControl', // perform access control for CRUD operations
+			'postOnly + delete', // we only allow deletion via POST request
+		);
+	}
+	
 	public function actionView($id) {
 		$this->render('view', array(
 			'model' => $this->loadModel($id, 'Station'),
